386d456b6e1077fd6396f09140d9488cbfd73356
josemanso <mansoortega@gmail.com>
2020-10-15 16:28:45 +0200

    wavfiles record show

---
+++ grabacion.py
@@ -0,0 +1,59 @@
+import sys
+import os
+import pyaudio
+import wave
+
+# entrada de argumentos
+try:
+    if len(sys.argv) == 1:
+        file_input = 'grabacion.wav'
+    else:
+        file_input = sys.argv[1]
+        print(sys.argv[1])
+except IOError as ex:
+    print(ex)
+    
+
+chunk = 4096 # 2^12 samples for buffer
+sample_format = pyaudio.paInt16 # 16 bits per sample
+channels = 1
+fs = 44100 # record at 44100 samples per second
+seconds = 10
+filename = '/home/pi/wavfiles/'+file_input
+#filename = file_input
+
+p = pyaudio.PyAudio() # Create an interface to PortAudio
+
+print('Recording')
+
+stream = p.open(format = sample_format,
+                channels = channels,
+                rate = fs,
+                input = True,
+                frames_per_buffer = chunk)
+
+frames = [] # Initialize array to store frames
+
+# Store data in chunks for 10 seconds
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+print('Finished recording')
+# Stop and close the stream
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio Interface
+p.terminate()
+
+# Save the recorded data as a wav file
+wf = wave.open(filename, 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+# plays the audio file
+os.system("aplay /home/pi/wavfiles/grabacion.wav")
+                
---
+++ showdata.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+
+# entrada de argumentos
+try:
+    if len(sys.argv) == 1:
+       file_input = 'grabacion.wav'
+       
+    else:
+        file_input = sys.argv[1]
+        print(sys.argv[1])
+except IOError as es:
+    print(ex)
+# excepción de fichero
+if os.path.isfile("/home/pi/wavfiles/"+file_input):
+    filename = "/home/pi/wavfiles/"+file_input
+    print('file exit')
+else:
+    print('Fiele not exit')
+    exit()
+    
+# lectura del archivo
+fs, audio = wavfile.read(filename)
+print('frecuencia: ', fs, ' Hz')
+print('audioshape: ',audio.shape, ', array longitud \nlongitud: ',
+      len(audio))
+print('datos: \n', audio)
+
+time = np.arange(len(audio))/fs
+plt.plot(time, audio)
+plt.title('Wave form')
+plt.xlabel('time, seconds')
+plt.ylabel('Amplitud')
+
+plt.show()
