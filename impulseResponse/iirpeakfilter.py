c2ecc2ecae1f825269e8f70c3d745a1c4df79a18
josemanso <mansoortega@gmail.com>
2021-03-26 11:19:27 +0100

    peak 2 orden

--- impulseResponse/iirpeakfilter.py
+++ impulseResponse/iirpeakfilter.py
@@ -7,7 +7,20 @@ fs = 1000.0  # Sample frequency (Hz)
 f0 = 300.0  # Frequency to be retained (Hz)
 Q = 30.0  # Quality factor
 # Design peak filter
-b, a = signal.iirpeak(f0/(fs/2), Q)
+def peakfilter(G, fc, fs, Q):
+    K = np.tan(np.pi*fc/fs)
+    V = 10**(G/20)
+    #b0 = (V + np.sqrt(2*V)*K+(K**2))/(1+np.sqrt(2)*K+K**2)
+    b0 = (1 +(V/Q)*K + K**2)/(1 + (K/Q) + K**2)
+    b1 = (2*((K**2)-1))/(1 + K/Q + K**2)
+    b2 = (1 - (V/Q)*K + K**2)/ (1 + K/Q + K**2)
+    a1 = (2*((K**2)-1))/(1 + K/Q + K**2)
+    a2 = (1-(K/Q) + (K**2))/(1 + K/Q + K**2)
+    A = [1, a1, a2] 
+    B = [b0, b1, b2]
+    return B,A
+# b, a = signal.iirpeak(f0/(fs/2), Q) # de primer orden
+b, a = peakfilter(1, f0, fs, Q)
 
 # Frequency response
 freq, h = signal.freqz(b, a)
